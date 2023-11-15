9e4fdef0d7ac0e0a03ba62900495a8e595033aab
afluffysquirrel <afluffysquirrel@gmail.com>
2023-11-15 11:28:34 +0000

    fix: pipeline error

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { test } from "@jest/globals";
 
 test("renders learn react link", () => {
   render(<App />);
